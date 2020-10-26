8e0996a72128a301280098ff3e3130502dc536be
HarperZack <harper.zack@gmail.com>
2020-10-26 09:01:27 -0400

    mvp achieved

--- app.py
+++ app.py
@@ -1,4 +1,4 @@
-from flask import Flask, render_template
+from flask import Flask, render_template, request, redirect
 import speech_recognition as sr
 
 """
@@ -9,5 +9,33 @@ https://www.youtube.com/watch?v=vuaolF-OSGY
 app = Flask(__name__)
 
 
+@app.route('/', methods=['GET', 'POST'])
+def index():
+    transcript = ''
+    if request.method == 'POST':
+        print('form data recieved')
+
+        if 'file' not in request.files:
+            return redirect(request.url)
+
+        file = request.files['file']
+        if file.filename == '':
+            return redirect(request.url)
+
+        if file:
+            # Thing that understands and transcribes
+            recognizer = sr.Recognizer()
+            audio_file = sr.AudioFile(file)
+            with audio_file as source:
+                # Original output for the given file
+                data = recognizer.record(source)
+            transcript = recognizer.recognize_google(data, key=None)
+            print(transcript)
+
+        return render_template('index.html', transcript=transcript)
+    else:
+        return render_template('index.html')
+
+
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(debug=True, threaded=True)
