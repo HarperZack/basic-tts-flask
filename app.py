0cc604c108fdca1756f1219c898333170df56165
HarperZack <harper.zack@gmail.com>
2020-10-25 22:19:47 -0400

    initial commit

--- app.py
+++ app.py
@@ -1,6 +1,11 @@
 from flask import Flask, render_template
 import speech_recognition as sr
 
+"""
+Tutorial is here. Posted July 31, 2020. Accessed October 25, 2020.
+https://www.youtube.com/watch?v=vuaolF-OSGY
+"""
+
 app = Flask(__name__)
 
 
